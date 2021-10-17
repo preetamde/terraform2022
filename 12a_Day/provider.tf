1bfe90e68e080ac8753b78edd4db8c0e0da202c2
preetamde <github@preetamzare.com>
2021-10-17 12:05:25 +0200

    added description to the profile

--- 12a_Day/provider.tf
+++ 12a_Day/provider.tf
@@ -1,4 +1,4 @@
 provider "aws" {
   region  = "us-east-2"
-  profile = "terraform2021" # name of the profile
+  profile = "terraform2021" # name of the profile, hence no need to define tfvars file here
 }
